ebdead2e358bd1933ee4179d45fa554540277ed0
Oscar <oscar.sobrevilla@gmail.com>
2016-03-30 18:41:27 -0500

    update

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-var fs = require('fs');
-var jade = require('jade');
+const fs = require('fs');
+const jade = require('jade');
+const glob = require('glob');
+const repConf = require('../../package.json').reporter;
 
-module.exports = function(source, destiny) {
-    var sourcePath = source || __dirname + '/report.json';
-    var destPath = destiny || './report.html';
-    var jsonReport = require(sourcePath);
-    var fn = jade.compileFile(__dirname + '/templates/report.jade', {
-      pretty: true
-    });
+const REG_MATCH_LINES = /@.+\:.+/g;
+const REG_MATCH_VALUES = /([\w-_]+)(?:(?:\s+)?\:(?:\s+)?)((?:\s?[\w-_])+)/i;
 
-    // Render the function
-    var html = fn({
-      report: jsonReport
-    });
-    write(destPath, html);
-}
+const fn = jade.compileFile(__dirname + '/templates/report.jade', {
+    pretty: true
+});
 
+const reporter = function(source, dest) {
+    dest   = './report.html' || dest;
+    source = './src/views/**/*.jade' || source;
+    var views = [],
+        files = glob.sync(source);
+    if (files)
+        files.forEach(function(file) {
+            var meta = {},
+                lines = (fs.readFileSync(file, {
+                    encoding: 'utf-8'
+                }).toString().match(REG_MATCH_LINES) ||  []);
+            lines.forEach(function(line) {
+                var splits = line.match(REG_MATCH_VALUES).slice(1, 4);
+                meta[splits[0]] = splits[1];
+            });
+            views.push(meta);
+        });
+
+    write(dest, fn({
+        title: repConf.title || "",
+        logo:  repConf.logo || "" ,
+        views: views
+    }));
+};
 
 /**
  * echo str > path.
@@ -30,3 +48,5 @@ function write(path, str, mode) {
     });
     console.log('   \x1b[36mcreate\x1b[0m : ' + path);
 }
+
+module.exports = reporter;
